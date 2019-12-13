d140012d9dcb948a97c00f0d1d2c19e87b93c801
Tammie Chung <tammie.chung11@gmail.com>
2019-12-13 16:21:33 -0500

    Research options for ttypewriter effect - return to this after

--- src/Components/Home/IntroText.js
+++ src/Components/Home/IntroText.js
@@ -2,22 +2,24 @@ import React, { useState, useEffect } from 'react';
 
 export default function IntroText(props) {
 
-  const [text, setText] = useState('');
+  const [text, setText] = useState();
 
   const intro = "Hi, my name is Tammie, and I'm a full-stack web developer."
 
-  useEffect(() => {
-    for (let i = 0; i < intro.length; i++) {
-      setTimeout(() => {
-        setText(prev => prev += intro[i])
-      }, 50 * (i + 1));
-    }
+  // useEffect(() => {
+  //   for (let i = 0; i < intro.length; i++) {
+  //     setTimeout(() => {
+  //       setText(prev => prev += intro[i])
+  //     }, 50 * (i + 1));
+  //   }
+
+  //   return () => clearTimeout()
   
-  }, [text])
+  // }, [text])
 
   return (
     <div className='typewriter'>
-      <h1>{text}</h1>
+      <h1>{intro}</h1>
     </div>
   );
 }
