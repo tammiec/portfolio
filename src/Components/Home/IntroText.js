2b7c61386428b7a53d4157cdf2ac15aefc27cfd8
Tammie Chung <tammie.chung11@gmail.com>
2019-12-13 16:14:04 -0500

    Experiment with typewriter effect

--- src/Components/Home/IntroText.js
+++ src/Components/Home/IntroText.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function IntroText(props) {
+
+  const [text, setText] = useState('');
+
+  const intro = "Hi, my name is Tammie, and I'm a full-stack web developer."
+
+  useEffect(() => {
+    for (let i = 0; i < intro.length; i++) {
+      setTimeout(() => {
+        setText(prev => prev += intro[i])
+      }, 50 * (i + 1));
+    }
+  
+  }, [text])
+
   return (
-    <h1>Hi, my name is Tammie, and I'm a full-stack web developer.</h1>
+    <div className='typewriter'>
+      <h1>{text}</h1>
+    </div>
   );
 }
