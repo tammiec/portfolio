bc502b5b02d914dda1c8a4e6139fa6159a3f6775
Tammie Chung <tammie.chung11@gmail.com>
2019-12-13 14:49:41 -0500

    Create button

---
+++ src/Components/Home/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Button(props) {
+
+  const handleClick = () => {
+
+  }
+
+  return (
+    <button onClick={handleClick}>Click here to learn more about me!</button>
+  );
+}
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import IntroText from './IntroText';
+import Button from './Button';
 
 export default function Component(props) {
   return (
     <div id='home'>
       <IntroText />
+      <Button />
     </div>
   );
 }
