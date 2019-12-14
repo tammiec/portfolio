8485d595611f5f5723239ee8b02e4d48b4e1d1f0
Tammie Chung <tammie.chung11@gmail.com>
2019-12-14 16:36:29 -0500

    Implement scrollToRef for navbar buttons

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
+import scrollToRef from '../Helpers/scrollToRef';
+
 import './NavBar.scss';
 
 export default function NavBar(props) {
+
+  const handleClick = item => {
+    scrollToRef(item)
+  }
   return (
     <nav id='nav-center'>
-      <p className='nav-item'>About Me</p>
-      <p className='nav-item'>Projects</p>
-      <p className='nav-item'>Contact</p>
+      <p className='nav-item' onClick={() => handleClick(props.aboutRef)}>About Me</p>
+      <p className='nav-item' onClick={() => handleClick(props.projectsRef)}>Projects</p>
+      <p className='nav-item' onClick={() => handleClick(props.contactRef)}>Contact</p>
     </nav>
   );
 }
