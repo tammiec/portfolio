9d28e34afe083cada0510cdb829c97e08d4bfb78
Tammie Chung <tammie.chung11@gmail.com>
2019-12-14 11:05:27 -0500

    Update about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,9 +8,16 @@ import './About.scss';
 export default function About(props) {
   return (
     <>
-      <h2>About Me</h2>
-      <img id='profile-picture' src={profilePicture} alt='Tammie Chung' />
-      <p>Magna reprehenderit excepteur quis culpa. Ullamco cillum aute pariatur in sit laborum adipisicing sint cillum cillum dolore sint ipsum. Ea ea velit sint deserunt. Est sit enim laboris tempor tempor exercitation qui. Dolor id consequat labore consequat aliqua tempor dolore ea dolore sunt. Esse occaecat cupidatat dolore sit ipsum occaecat consequat commodo.</p>
+      <div id='bio-container'>
+        <div id='bio-photo'>
+          <h2>About Me</h2>
+          <img id='profile-picture' src={profilePicture} alt='Tammie Chung' />
+        </div>
+        <p>
+          I'm a web developer based in Toronto with a background in law, event management, and customer service. I'm passionate about learning new skills, and love exercising my creativity, which is what led me to pursue a career in web development. When I'm not busy coding, you can find me trying out the best cheap eats in the city, playing piano and violin, or just curled up on the couch with my cat and a good book.
+        </p>
+      </div>
+      
       <h2>Technical Skills</h2>
       <TechList 
         skillType='languages'
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,10 +4,10 @@ import './NavBar.scss';
 
 export default function NavBar(props) {
   return (
-    <header id='nav-center'>
+    <nav id='nav-center'>
       <p className='nav-item'>About</p>
       <p className='nav-item'>Projects</p>
       <p className='nav-item'>Contact</p>
-    </header>
+    </nav>
   );
 }
