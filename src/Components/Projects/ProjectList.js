cb39aa231fc0989f24749ac25843f563bd97d265
Tammie Chung <tammie.chung11@gmail.com>
2019-12-14 21:30:58 -0500

    Import project screenshots

--- src/Components/Projects/ProjectList.js
+++ src/Components/Projects/ProjectList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import ProjectListItem from './ProjectListItem';
-import placeholder from './Diagram.png';
+
+import drawingBoard from './project-drawingboard.png';
+import toodleedoo from './project-toodleedoo.png';
+import tweeter from './project-tweeter.png';
 
 export default function ProjectList(props) {
 
@@ -9,17 +12,17 @@ export default function ProjectList(props) {
     {
       name: 'the Drawing Board',
       description: "A collaborative meeting app that allows users to schedule and host meetings remotely, whiteboard over documents in real-time, and take personal notes. Audio streaming and text chats are enabled in the meeting room for communication, and when the meeting ends, marked up documents and notes are saved to users' dashboards for future reference.",
-      image: placeholder
+      image: drawingBoard
     },
     {
       name: 'Toodleedoo',
       description: 'Toodleedoo is a smart to-do list application that helps you organize and sort your tasks. Upon categorization, Toodleedoo will suggest three resources for the user to check out. The user has the ability to edit the name of their task, add a description, add additional resources, mark it as complete, mark it as important, and send the task to another local user.',
-      image: placeholder
+      image: toodleedoo
     },
     {
       name: 'Tweeter',
       description: 'A single-page Twitter clone project powered by Ajax.',
-      image: placeholder
+      image: tweeter
     }
   ]
 
