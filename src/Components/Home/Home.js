ef84a2edea8215672b732ff0f9b05305931c9245
Tammie Chung <tammie.chung11@gmail.com>
2020-04-20 17:25:36 -0400

    Fix hover and refactor to simplify

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import IntroText from './IntroText';
 import scrollToRef from '../../Helpers/scrollToRef';
@@ -8,23 +8,15 @@ import './Home.scss';
 export default function Component(props) {
 
   const [textLoaded, setTextLoaded] = useState(false);
-  const [fadeIn, setFadeIn] = useState(false);
 
   const handleClick = () => {
     scrollToRef(props.aboutRef)
   }
 
-  useEffect(() => {
-    if (textLoaded) {
-      setFadeIn(true)
-    }
-  
-  }, [textLoaded])
-
   return (
     <>
       <IntroText setTextLoaded={setTextLoaded} />
-      <button className={`about-me-button ${fadeIn && 'fade-in'}`} onClick={handleClick}>About Me<span>&#8594;</span></button>
+      <button className={`about-me-button ${textLoaded && 'fade-in'} ${textLoaded && 'button-hover'}`} onClick={handleClick}>About Me<span>&#8594;</span></button>
     </>
   );
 }
