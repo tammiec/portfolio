dc1b895f725da62462f1039a86e35ab315cf1684
Tammie Chung <tammie.chung11@gmail.com>
2020-04-30 13:57:31 -0400

    added extra placeholder cards

--- src/Components/Projects/project-details.js
+++ src/Components/Projects/project-details.js
@@ -4,6 +4,11 @@ import tweeter from './project-tweeter.png';
 
 export const projects = [
   {
+    name: 'GigEco',
+    description: "An Order Management System designed for contract employee management in the insurance industry. The system was built to accommodate the admin staff, companies/agencies, agents, and contractors, with separate portals for each type of user. Order forms, lists, user management, invoicing, and payslip management can all be handled through this system.",
+    image: null
+  },
+  {
     name: 'the Drawing Board',
     description: "A collaborative meeting app that allows users to schedule and host meetings remotely, whiteboard over documents in real-time, and take personal notes. Audio streaming and text chats are enabled in the meeting room for communication, and when the meeting ends, marked up documents and notes are saved to users' dashboards for future reference.",
     image: drawingBoard
@@ -17,5 +22,10 @@ export const projects = [
     name: 'Tweeter',
     description: 'A single-page Twitter clone project powered by Ajax.',
     image: tweeter
-  }
+  },
+  {
+    name: 'calculator',
+    description: "A basic calculator built using React Native",
+    image: null
+  },
 ]
